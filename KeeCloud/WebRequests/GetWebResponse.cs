2fd63d7220bf23d99180d575cab6b10f3d02b479
Markus Rathgeb <maggu2810@gmail.com>
2015-12-20 17:01:17 +0100

    implement close to prevent exception on save

--- KeeCloud/WebRequests/GetWebResponse.cs
+++ KeeCloud/WebRequests/GetWebResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net;
 
 namespace KeeCloud.WebRequests
@@ -18,5 +19,20 @@ namespace KeeCloud.WebRequests
 
         // mono needs this but .NET does not
         public override WebHeaderCollection Headers { get { return null; } }
+
+		public override void Close()
+		{
+			// WebReponse.Close() must not be called
+			// System.NotSupportedException: Specified method is not supported.
+			// at System.Net.WebResponse.Close () [0x00000] in /.../mono-4.2.1/mcs/class/System/System.Net/WebResponse.cs:100 
+			try
+			{
+				this.stream.Close();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+		}
     }
 }
--- KeeCloud/WebRequests/SuccessWebResponse.cs
+++ KeeCloud/WebRequests/SuccessWebResponse.cs
@@ -16,5 +16,21 @@ namespace KeeCloud.WebRequests
         {
             this.stream.Dispose();
         }
+
+		public override void Close()
+		{
+			// WebReponse.Close() must not be called
+			// System.NotSupportedException: Specified method is not supported.
+			// at System.Net.WebResponse.Close () [0x00000] in /.../mono-4.2.1/mcs/class/System/System.Net/WebResponse.cs:100 
+			try
+			{
+				this.stream.Close();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+		}
+
     }
 }
